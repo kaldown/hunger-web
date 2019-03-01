2979965c6d71ed9aecb4505f86cf809984ac88e0
kAldown <kaldownb@gmail.com>
2019-03-01 14:09:17 +0300

    setstate func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Quiz from './Quiz';
 
+const {QuizRequest} = require('./proto/quiz_pb.js');
+const {QuizerClient} = require('./proto/quiz_grpc_web_pb.js');
+
 class App extends Component {
 	initial = {
 		quizTitle: "No name",
@@ -46,11 +49,31 @@ class App extends Component {
 	}
 
 	answer = key => {
-		window.alert("Wrong answer " + key);
+		this.setState(_getQuiz);
+	}
+}
 
-		this.setState({
-			quiz: this.initial
-		});
+function _getQuiz(state, props) {
+	var client = new QuizerClient('http://localhost:8080', null, null);
+
+	var request = new QuizRequest();
+	request.setMessage('salo');
+
+	client.getQuiz(request, {}, (err, response) => {
+		if (err) {
+			alert(err.message);
+		} else {
+			alert(response.getMessage());
+		}
+	});
+
+	return {
+		quiz: {
+			quizTitle: "Answer",
+			quizVariants: {
+				1: "xz"
+			}
+		}
 	}
 }
 
